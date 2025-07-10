661a5a3057263aa6d92dde5d6bffda72a18cd4b0
Gokulmani <113236487+Gokulmani1@users.noreply.github.com>
2025-07-10 17:20:16 +0530

    Update BattleRuleBook.js  Terrain

--- server/models/BattleRuleBook.js
+++ server/models/BattleRuleBook.js
@@ -1,6 +1,8 @@
+const terrain = require("./terrain");
+
 class BattleRuleBook {
 
- static advantageMap = {
+  static advantageMap = {
     Militia: ['Spearmen', 'LightCavalry'],
     Spearmen: ['LightCavalry', 'HeavyCavalry'],
     LightCavalry: ['FootArcher', 'CavalryArcher'],
@@ -8,17 +10,29 @@ class BattleRuleBook {
     CavalryArcher: ['Spearmen', 'HeavyCavalry'],
     FootArcher: ['Militia', 'CavalryArcher'],
   };
-
-   warField(own, enemy) {
+  
+    warField(own, enemy) {
+    let terrainAdvantage = terrain.terrainAdvantage(own, enemy);
+    let result = -1;
+    // let ownterrainAdvantage = 0;
+    // let enemyterrainAdvantage = 0;
+    let ownStrength = terrainAdvantage.ownStrength;
+    let enemyStrength = terrainAdvantage.enemyStrength;
+    // if (own.terrain != "Default") {
+    //   ownterrainAdvantage = BattleRuleBook.terrainMap[own.terrain][own.unitClass]?.includes(terrain) ? (BattleRuleBook.terrainMap[own.terrain][own.unitClass]) : (0);
+    //   enemyterrainAdvantage = BattleRuleBook.terrainMap[enemy.terrain][enemy.unitClass]?.includes(terrain) ? (BattleRuleBook.terrainMap[enemy.terrain][enemy.unitClass]) : (0);
+    //   if (ownterrainAdvantage != 0) ownStrength =terrain.ownterrainAdvantage
+    //   if (enemyterrainAdvantage != 0) enemyStrength = enemyStrength * enemyterrainAdvantage;
+    // }
     const ownAdvantage = BattleRuleBook.advantageMap[own.unitClass]?.includes(enemy.unitClass);
     const enemyAdvantage = BattleRuleBook.advantageMap[enemy.unitClass]?.includes(own.unitClass);
-    let ownStrength = own.count;
-    let enemyStrength = enemy.count;
+    ownStrength = own.count;
+    enemyStrength = enemy.count;
     if (ownAdvantage) ownStrength *= 2;
     if (enemyAdvantage) enemyStrength *= 2;
-    if (ownStrength > enemyStrength) return 1;
-    if (ownStrength === enemyStrength) return 0;
-    return -1;
+    if (ownStrength > enemyStrength) result = 1;
+    if (ownStrength === enemyStrength) result = 0;
+    return result;
   }
 }
 
